a4e01adec23b45336ee5f7b4544b4a08493797bf
dclatzen <devon@studyblue.com>
2016-10-28 18:24:08 -0700

    Initial Commit

---
+++ recommended_cards_proto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recommended_cards_proto
+//
+//  Created by StudyBlue on 10/28/16.
+//  Copyright © 2016 myself. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
