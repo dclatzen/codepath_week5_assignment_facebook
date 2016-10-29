43e4e51e88ac404bf9689e86bd13cf77782c1a7c
dclatzen <devon@studyblue.com>
2016-10-28 20:54:42 -0700

    Working on connecting the tray to the suggested card movement.

--- SuggestionViewController.swift
+++ SuggestionViewController.swift
@@ -14,17 +14,28 @@ class SuggestionViewController: UIViewController {
     
     @IBOutlet weak var suggestedCardParentView: UIView!
     @IBOutlet weak var suggestedCard: UIImageView!
+    @IBOutlet weak var suggestedCardTray: UIImageView!
     
     var suggestedParentOriginalX: CGFloat!
+    var suggestionsTranslateX: CGFloat!
+    
     var halfWay: CGFloat!
     var didPanHalfWayRight: Bool!
     var didPanHalfWayLeft: Bool!
+    
+    var suggestedTrayOriginalY: CGFloat!
+    var suggestedTrayUp: CGFloat!
+    var suggestedTrayDown: CGFloat!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        suggestedParentOriginalX = suggestedCardParentView.center.x
+        
+        print ("suggestedParentOriginalX: \(Double(suggestedParentOriginalX))")
+        
     }
     
     
@@ -49,13 +60,14 @@ class SuggestionViewController: UIViewController {
             suggestedCardParentView.center.x = suggestedParentOriginalX + translation.x
             
         } else if sender.state == .ended {
-            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [], animations: {
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseOut], animations: {
                 
                 if self.didPanHalfWayLeft! {
                     self.suggestedCardParentView.center.x = self.suggestedParentOriginalX - (self.suggestedCard.frame.width + 2)
                     print ("Did pan halfway left.")
                 } else if self.didPanHalfWayRight! {
-                    self.suggestedCardParentView.center.x = self.suggestedParentOriginalX + (self.suggestedCard.frame.width - 2)
+                    self.suggestedCardParentView.center.x = self.suggestedParentOriginalX + (self.suggestedCard.frame.width + 2)
                     print("Did pan halfway right")
                 } else {
                     self.suggestedCardParentView.center.x = self.suggestedParentOriginalX
@@ -70,7 +82,69 @@ class SuggestionViewController: UIViewController {
     } // end didPanSuggestedCardParent
     
 
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        
+        let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        let isMovingDown = velocity.y > 0
+        
+        suggestedParentOriginalX = suggestedCardParentView.center.x
+        
+        suggestedTrayUp = 176
+        
+        // the tray moves down by the height of the keyboard
+        suggestedTrayDown = 392
+        
+        if sender.state == .began {
+            
+            suggestedTrayOriginalY = suggestedCardTray.frame.origin.y
+            
+        } else if sender.state == .changed {
+            
+            if isMovingDown {
+                
+                suggestedCardTray.frame.origin.y = suggestedTrayOriginalY + translation.y
+                
+                // move suggestions out of the way as the tray moves down
+                suggestionsTranslateX = convertValue(inputValue: translation.x, r1Min: suggestedTrayUp, r1Max: suggestedTrayDown, r2Min: 1, r2Max: 100)
+                
+                suggestedCardParentView.center.x = suggestedParentOriginalX - suggestionsTranslateX
+                
+                print ("Moving tray down.")
+                print ("suggestionsTranslateX: \(Double(suggestionsTranslateX))")
+                print ("suggestedCardParentView.center.x = \(Double(suggestedCardParentView.center.x))")
+                
+            } else {
+                
+                // if the tray is moving up, don't bring suggestions in until the gesture ends
+                suggestedCardTray.frame.origin.y = suggestedTrayOriginalY + translation.y
+            }
+            
+        } else if sender.state == .ended {
+            
+            if isMovingDown {
+                
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseOut], animations: {
+                    self.suggestedCardTray.frame.origin.y = self.suggestedTrayDown
+                    self.suggestedCardParentView.center.x = self.suggestedParentOriginalX + 375
+                    
+                })
+                
+                
+            } else {
+                
+                UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseOut], animations: {
+                    self.suggestedCardTray.frame.origin.y = self.suggestedTrayUp
+                    self.suggestedCardParentView.center.x = self.suggestedParentOriginalX - 375
+                })
 
+            }
+            
+        }
+        
+        
+    }
+    
     
 
 }
